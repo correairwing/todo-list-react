9f4dd48ce917f5baa712c0efc6cefcd49590554e
Irwing <irwingcorrea95@gmail.com>
2023-08-07 09:57:40 -0300

    procurando o bug na cor da tag

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,44 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
+import * as enums from '../../utils/enums/Tarefa'
+
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'Pagar a conta de luz',
+    descricao: 'Baixar a fatura no app',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'Treino B',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  }
+]
+
+const ListaDeTarefas = () => (
+  <Container>
+    <ul>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
+    </ul>
+  </Container>
+)
+
+export default ListaDeTarefas
